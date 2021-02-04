c13733d833f740e06b84c198283a0eeb0269821c
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-04 09:10:15 +0000

    updates

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ import pprint
 key = os.environ['key']
 token = os.environ['token']
 
-payload = {
-    'appid': key,
-    'q': 'Samarqand'
-}
-url = f'https://api.openweathermap.org/data/2.5/weather'
-respons = requests.get(url=url, params=payload)
-print(respons.json())
-print(respons.url)
+def Updates():
+    url_update = f'https://api.telegram.org/bot{token}/getUpdates'
+    respons = requests.get(url=url_update)
+    data = respons.json()['result']
+    last_msg = data[-1]
+
+    return last_msg
+
+# payload = {
+#     'appid': key,
+#     'q': 'Bulung\'ur'
+# }
+# url = f'https://api.openweathermap.org/data/2.5/weather'
+# respons = requests.get(url=url, params=payload)
+# print(respons.json())
+# print(respons.url)
