40da493965088ed0d4cdb8e163b43fb9589ab61e
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-04 09:17:35 +0000

    Wether data

--- main.py
+++ main.py
@@ -7,17 +7,34 @@ token = os.environ['token']
 
 def Updates():
     url_update = f'https://api.telegram.org/bot{token}/getUpdates'
-    respons = requests.get(url=url_update)
-    data = respons.json()['result']
+    respons_u = requests.get(url=url_update)
+    data = respons_u.json()['result']
     last_msg = data[-1]
 
     return last_msg
 
-# payload = {
-#     'appid': key,
-#     'q': 'Bulung\'ur'
-# }
-# url = f'https://api.openweathermap.org/data/2.5/weather'
-# respons = requests.get(url=url, params=payload)
-# print(respons.json())
-# print(respons.url)
+
+def Weather(text):
+    payload = {
+        'appid': key,
+        'q': text
+    }
+    url_w = f'https://api.openweathermap.org/data/2.5/weather'
+    respons_w = requests.get(url=url_w, params=payload)
+    data_w = respons_w.json()
+
+
+
+
+
+msg_id = Updates()['message']['message_id']
+
+while True:
+    data = Updates()
+    last_msg_id = data['message']['message_id']
+    msg_text = data['message']['text']
+
+    if msg_id != last_msg_id:
+        weather_data = Weather(msg_text)
+
+
