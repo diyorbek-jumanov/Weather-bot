1cc3612e8f092855db7009237abbc7ff29a29a8d
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-04 06:14:15 +0000

    data

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import os
 import pprint
 
 key = os.environ['key']
-
 token = os.environ['token']
 
-print(token)
+payload = {
+    'appid': key,
+    'q': 'Samarqand'
+}
+url = f'https://api.openweathermap.org/data/2.5/weather'
+respons = requests.get(url=url, params=payload)
+print(respons.json())
+print(respons.url)
