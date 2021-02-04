1fa79fe8d026e166d6c35ba4f073efee92edb020
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-02-04 09:35:23 +0000

    data send

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ def Weather(text):
     respons_w = requests.get(url=url_w, params=payload)
     data_w = respons_w.json()
 
+    full_data_w = list()
+    full_data_w.append(data_w['name'])
+    full_data_w.append(data_w['main']['temp'] - 273)
+    full_data_w.append(data_w['weather']['description'])
+    full_data_w.append(data_w['weather']['icon'])
+    full_data_w.append(data_w['wind']['speed'])
+
+    return full_data_w
 
 
 
@@ -35,6 +43,16 @@ while True:
     msg_text = data['message']['text']
 
     if msg_id != last_msg_id:
-        weather_data = Weather(msg_text)
+        w_d = Weather(msg_text)
+        w_from = w_d[0]
+        w_temp = w_d[1]
+        w_description = w_d[2]
+        w_icon = w_d[3]
+        w_w = w_d[4]
+        
+        send_msg_text = f"from: {w_from}\nTemp: {w_temp} {w_icon}\ndescription: {w_description}\nWind: {w_w}"
+
+        
+
 
 
